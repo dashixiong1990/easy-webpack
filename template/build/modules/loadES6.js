09c722c19e5ef143c1f678961a03d7e3c89540f0
bingomvm <bingomvm@163.com>
2017-07-04 13:12:58 +0800

    根据选择支持的浏览器初始化babel的配置

---
+++ template/build/modules/loadES6.js
@@ -0,0 +1,41 @@
+module.exports = ({ include, exclude } = {}) => {
+  let defaults = {
+    module: {
+      rules: [
+        {{#if_eq compatibility "IE6"}}
+          {
+            test: /.js$/,
+            loader: "es3ify-loader"
+          },
+        {{/if_eq}}
+        {
+          test:/\.js$/,
+          exclude:/node_modules/,
+          use:[
+            {
+              loader:"babel-loader",
+              options:{
+                {{#if_eq compatibility "modern"}}
+                  presets:["es2015"]
+                {{/if_eq}}
+                {{#if_eq compatibility "IE6"}}
+                  presets: [
+                    ["env", {
+                        "targets": {
+                            "browsers": ["ie 6-8"]
+                        },
+                        "useBuiltIns": true
+                    }]
+                  ]
+                {{/if_eq}}
+              }
+          }]
+        }
+      ]
+    }
+  }
+  if (process.env.NODE_ENV === 'production') {
+    defaults.plugins = [extractPlugin]
+  }
+  return defaults
+}
